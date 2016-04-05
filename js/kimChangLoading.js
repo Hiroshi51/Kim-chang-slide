91880ff2992e402d8f659784efe208ae4383e7af
Hiroshi51 <hiroshigoibuchi0@gmail.com>
2016-04-05 16:27:39 +0900

    Update kimChangLoading.js

--- js/kimChangLoading.js
+++ js/kimChangLoading.js
@@ -1,9 +1,4 @@
-$(function(){
-	    $('#wrap').css({display:"none"});	
-	    $('#loader-bg').animate({width:"100%"},900);
-	    });
-
-		$(window).load(function () { 
-		  $('#loader-bg').delay(100).animate({height:"100%"},900).fadeOut(500)
-		  var show = window.setTimeout(function(){$('#wrap').fadeIn()},1700);
+$(window).load(function () { 
+		  $('#loader-bg').delay(900).fadeOut(500)
+		  
 		});
