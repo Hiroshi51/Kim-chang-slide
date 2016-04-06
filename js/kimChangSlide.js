ab6eb8266b09df8242abb25fe20bc2ace60964c1
Hiroshi51 <hiroshigoibuchi0@gmail.com>
2016-04-06 10:17:47 +0900

    Added files via upload

--- js/kimChangSlide.js
+++ js/kimChangSlide.js
@@ -80,76 +80,81 @@ $(document).ready(function(){
   //次への動作関数
   function animateSlideNext(){
        	switch(slidePosition){
-			case 0:
-			    moveSlideForward()
-				$('.fadeIntext01').animate({top:"100px",opacity:"0"},500);
-				var wait = window.setTimeout(function(){
-				$('.fadeIntext02').animate({top:"20px",opacity:"1"});
-				},10);
-			    slidePosition++;
-			    break;
-			case 1:
-			 	moveSlideForward()
-			    $('.fadeIntext02').animate({top:"100px",opacity:"0"},500);
-				var wait = window.setTimeout(function(){
-				$('.fadeIntext03').animate({top:"20px",opacity:"1"});
-				},10);
-			    slidePosition++;
-			    break;   
-			case 2:
-			 	moveSlideForward()
-			    $('.fadeIntext03').animate({top:"100px",opacity:"0"},500);
-				var wait = window.setTimeout(function(){
-				$('.fadeIntext04').animate({top:"20px",opacity:"1"});
-				},10);
-			    slidePosition++;
-			    break;       
-			case 3:
-			 	$('#mainSlide').animate({left:sliderMargin*1.5+adjustMargin},500);
-			 	$('.fadeIntext04').animate({top:"100px",opacity:"0"},500);
-				var wait = window.setTimeout(function(){
-				$('.fadeIntext01').animate({top:"20px",opacity:"1"});
-				},10);
-			    slidePosition = 0;
-			    break;  
+        case 0:
+        moveSlideForward()
+        $('.fadeIntext01').animate({top:"100px",opacity:"0"},500);
+        var wait = window.setTimeout(function(){
+        $('.fadeIntext02').animate({top:"20px",opacity:"1"});
+        },10);
+        slidePosition++;
+        break;
+        case 1:
+        moveSlideForward()
+        $('.fadeIntext02').animate({top:"100px",opacity:"0"},500);
+        var wait = window.setTimeout(function(){
+        $('.fadeIntext03').animate({top:"20px",opacity:"1"});
+        },10);
+        slidePosition++;
+        break;   
+        case 2:
+        moveSlideForward()
+        $('.fadeIntext03').animate({top:"100px",opacity:"0"},500);
+        var wait = window.setTimeout(function(){
+        $('.fadeIntext04').animate({top:"20px",opacity:"1"});
+        },10);
+        slidePosition++;
+        break;       
+        case 3:
+        if(windowWidth > 600){
+          $('#mainSlide').animate({left:sliderMargin*1.5+adjustMargin},500);
+        }
+        else{
+          $('#mainSlide').animate({left:sliderMargin},500);
+        }
+        $('.fadeIntext04').animate({top:"100px",opacity:"0"},500);
+        var wait = window.setTimeout(function(){
+        $('.fadeIntext01').animate({top:"20px",opacity:"1"});
+        },10);
+        slidePosition = 0;
+        break;  
 		}
   }	
 
   //前への動作関数
   function animateSlideBack(){
-  	    switch(slidePosition){
-			case 0:
-                $('#mainSlide').animate({left:parseInt($('#mainSlide').css("left"))-slideWidth*3+"px"},500);
-                $('.fadeIntext01').animate({top:"100px",opacity:"0"},500);
-				var wait = window.setTimeout(function(){
-				$('.fadeIntext04').animate({top:"20px",opacity:"1"});
-				},10);
-			    slidePosition = 3;
-			    break;
-			case 1:
-			 	moveSlideBackward();
-			    $('.fadeIntext02').animate({top:"100px",opacity:"0"},500);
-				var wait = window.setTimeout(function(){
-				$('.fadeIntext01').animate({top:"20px",opacity:"1"});
-				},10);
-			    slidePosition--;
-			    break;   
-			case 2:
-			 	moveSlideBackward();
-			    $('.fadeIntext03').animate({top:"100px",opacity:"0"},500);
-				var wait = window.setTimeout(function(){
-				$('.fadeIntext02').animate({top:"20px",opacity:"1"});
-				},10);
-			    slidePosition--;
-			    break;       
-			case 3:
-			 	moveSlideBackward();
-			 	$('.fadeIntext04').animate({top:"100px",opacity:"0"},500);
-				var wait = window.setTimeout(function(){
-				$('.fadeIntext03').animate({top:"20px",opacity:"1"});
-				},10);
-			    slidePosition--;
-			    break;  
+        switch(slidePosition){
+        case 0:
+        $('#mainSlide').animate({left:parseInt($('#mainSlide').css("left"))-slideWidth*3+"px"},500);
+        $('.fadeIntext01').animate({top:"100px",opacity:"0"},500);
+        var wait = window.setTimeout(function(){
+        $('.fadeIntext04').animate({top:"20px",opacity:"1"});
+        },10);
+        slidePosition = 3;
+        break;
+        case 1:
+        moveSlideBackward();
+        $('.fadeIntext02').animate({top:"100px",opacity:"0"},500);
+        var wait = window.setTimeout(function(){
+        $('.fadeIntext01').animate({top:"20px",opacity:"1"});
+        },10);
+        slidePosition--;
+        break;   
+        case 2:
+        moveSlideBackward();
+        $('.fadeIntext03').animate({top:"100px",opacity:"0"},500);
+        var wait = window.setTimeout(function(){
+        $('.fadeIntext02').animate({top:"20px",opacity:"1"});
+        },10);
+        slidePosition--;
+        break;       
+        case 3:
+        moveSlideBackward();
+        $('.fadeIntext04').animate({top:"100px",opacity:"0"},500);
+        var wait = window.setTimeout(function(){
+        $('.fadeIntext03').animate({top:"20px",opacity:"1"});
+        },10);
+        slidePosition--;
+        break;  
      	}
    }
 
